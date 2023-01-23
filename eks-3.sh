708f0097059c02957dbb4f463dc193c6801ac76a
sdjo <darkquiz@gmail.com>
2023-01-23 12:48:58 +0900

    ...

---
+++ eks-3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "##################################################"
+echo "###start eks setting    start#####################"
+echo "##################################################"
+echo " "
+echo "This script pid : $$"
+echo "EFS Setting start"
+aws iam create-policy --policy-name AmazonEKS_EFS_CSI_Driver_Policy --policy-document file://iam-policy-example.json
+eksctl utils associate-iam-oidc-provider --region=$AWS_REGION --cluster=$CLUSTER_NAME --approve
+echo eksctl create iamserviceaccount --name efs-csi-controller-sa --namespace kube-system --cluster $CLUSTER_NAME --attach-policy-arn arn:aws:iam::$ACCOUNT_ID:policy/AmazonEKS_EFS_CSI_Driver_Policy --approve --override-existing-serviceaccounts --region $AWS_REGION
+eksctl create iamserviceaccount --name efs-csi-controller-sa --namespace kube-system --cluster $CLUSTER_NAME --attach-policy-arn arn:aws:iam::$ACCOUNT_ID:policy/AmazonEKS_EFS_CSI_Driver_Policy --approve --override-existing-serviceaccounts --region $AWS_REGION
+echo "set dirver.yaml"
+cd /root/team_prj_k8s/environment/efs
+echo "kubectl apply driver"
+kubectl apply -f driver.yaml
+sleep 10s
+echo "storageclasee id set"
+kubectl apply -f storageclass.yaml
+sleep 10s
+echo "pvc deply"
+kubectl apply -f pvc.yaml
+
